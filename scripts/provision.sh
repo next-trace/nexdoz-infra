53c2fe0a1e3a46e298c501aaa25b418e4f6ed4b1
hossein bagherzadegan talkhouncheh <75850730+hbttundar@users.noreply.github.com>
2026-04-24 14:41:30 +0200

    ci: suppress shellcheck SC1091 note on external sourcing + lower severity to warning

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -43,6 +43,7 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
+# shellcheck disable=SC1091
 . /etc/os-release
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable" \
   > /etc/apt/sources.list.d/docker.list
